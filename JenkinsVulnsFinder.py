69fbfb6d8b166f3634040ddf8494297288732f79
Bhanu <Bhanunamikaze@gmail.com>
2025-01-02 20:57:15 +0530

    Update JenkinsVulnsFinder.py
    Added --cookie based authentication, updated list of urls for Unauth endpoints

--- JenkinsVulnsFinder.py
+++ JenkinsVulnsFinder.py
@@ -4,17 +4,28 @@ import argparse
 import csv
 from datetime import datetime
 
-def detect_auth_type(base_url, auth):
-    auth_url = f"{base_url}/user/api/json"
-    response = requests.get(auth_url, auth=auth)
-    if response.status_code == 403:
-        return "Matrix-based Security"
-    elif response.status_code == 200:
-        return "RBAC or OAuth"
-    return "Unknown"
+def detect_auth_type(base_url, auth, headers):
+    auth_url = f"{base_url}/securityRealm/commenceLogin?from=%2F"
+    try:
+        response = requests.get(auth_url, auth=auth, headers=headers, allow_redirects=False)
+        print(response.status_code)
+        if response.status_code == 404:
+            return "Matrix-Based Authorization"
+        if response.status_code == 302:
+            response = requests.get(auth_url, auth=auth, headers=headers, allow_redirects=True)
+            # Update you company name here
+            if "Company Name" in response.text:
+                    return "Company OAuth"
+            return "Microsoft OAuth"
+        elif response.status_code == 200:
+            return "RBAC or OAuth"
+        return "Unknown"
+    except requests.RequestException as e:
+        #print(f"Error during authentication type detection: {e}")
+        return "Error Occured"
 
-def get_jenkins_version(base_url, auth):
-    response = requests.get(base_url, auth=auth)
+def get_jenkins_version(base_url, auth, headers):
+    response = requests.get(base_url, auth=auth, headers=headers)
     if "X-Jenkins" in response.headers:
         return response.headers.get("X-Jenkins")
     return "Unknown"
@@ -23,22 +34,22 @@ def check_anonymous_access(base_url):
     response = requests.get(base_url)
     return response.status_code == 200
 
-def check_plugins(base_url, auth):
+def check_plugins(base_url, auth, headers):
     plugins_url = f"{base_url}/pluginManager/api/json?depth=1"
-    response = requests.get(plugins_url, auth=auth)
+    response = requests.get(plugins_url, auth=auth, headers=headers)
     if response.status_code == 200:
         plugins = response.json().get("plugins", [])
         return [(plugin['shortName'], plugin['version'], plugin.get('hasUpdate', False)) for plugin in plugins]
     return []
 
-def check_csrf_protection(base_url, auth):
+def check_csrf_protection(base_url, auth, headers):
     crumb_url = f"{base_url}/crumbIssuer/api/json"
-    response = requests.get(crumb_url, auth=auth)
+    response = requests.get(crumb_url, auth=auth, headers=headers)
     return response.status_code == 200
 
-def check_job_visibility(base_url, auth):
+def check_job_visibility(base_url, auth, headers):
     jobs_url = f"{base_url}/api/json?tree=jobs[name,url]"
-    response = requests.get(jobs_url, auth=auth)
+    response = requests.get(jobs_url, auth=auth, headers=headers)
     if response.status_code == 200:
         jobs = response.json().get("jobs", [])
         job_details = []
@@ -49,15 +60,14 @@ def check_job_visibility(base_url, auth):
         return job_details
     return []
 
-def check_script_console(base_url, auth):
+def check_script_console(base_url, auth, headers):
     console_url = f"{base_url}/script"
-    response = requests.get(console_url, auth=auth)
+    response = requests.get(console_url, auth=auth, headers=headers)
     return response.status_code == 200
 
-def check_nodes(base_url, auth):
+def check_nodes(base_url, auth, headers):
     nodes_url = f"{base_url}/computer/api/json"
-    response = requests.get(nodes_url, auth=auth)
-    
+    response = requests.get(nodes_url, auth=auth, headers=headers)
     if response.status_code == 200:
         try:
             nodes = response.json().get("computer", [])
@@ -72,16 +82,14 @@ def check_nodes(base_url, auth):
             return {"num_nodes": len(nodes), "nodes": node_details}
         except ValueError:
             print(f"Error: Response from {nodes_url} is not valid JSON")
- 
-    
     return {"num_nodes": 0, "nodes": []}
 
 def check_http_usage(base_url):
     return base_url.lower().startswith("http://")
 
-def check_stored_credentials(base_url, auth):
+def check_stored_credentials(base_url, auth, headers):
     creds_url = f"{base_url}/credentials/store/system/domain/_/api/json?depth=1"
-    response = requests.get(creds_url, auth=auth)
+    response = requests.get(creds_url, auth=auth, headers=headers)
     if response.status_code == 200:
         credentials = response.json().get("credentials", [])
         creds_details = []
@@ -93,24 +101,24 @@ def check_stored_credentials(base_url, auth):
         return creds_details
     return []
 
-def check_log_access(base_url, auth):
+def check_log_access(base_url, auth, headers):
     log_url = f"{base_url}/log/all"
-    response = requests.get(log_url, auth=auth)
+    response = requests.get(log_url, auth=auth, headers=headers)
     return response.status_code == 200
 
-def check_audit_logs(base_url, auth):
+def check_audit_logs(base_url, auth, headers):
     audit_url = f"{base_url}/audit/api/json"
-    response = requests.get(audit_url, auth=auth)
+    response = requests.get(audit_url, auth=auth, headers=headers)
     return response.status_code == 200
 
-def check_api_access(base_url, auth):
+def check_api_access(base_url, auth, headers):
     api_url = f"{base_url}/api/json"
-    response = requests.get(api_url, auth=auth)
+    response = requests.get(api_url, auth=auth, headers=headers)
     return response.status_code == 200
 
-def check_cli_access(base_url, auth):
+def check_cli_access(base_url, auth, headers):
     cli_url = f"{base_url}/cli"
-    response = requests.get(cli_url, auth=auth)
+    response = requests.get(cli_url, auth=auth, headers=headers)
     return response.status_code == 200
 
 def check_anonymous_node_access(base_url):
@@ -125,8 +133,11 @@ def check_signup_allowed(base_url):
 
 def check_unauthenticated_endpoints(base_url):
     common_endpoints = [
-        "/", "/login", "/signup", "/script", "/api/json", "/computer/api/json",
-        "/env-vars.html", "/pluginManager/api/json", "/credentials/api/json", "/log/all"
+        "/", "/login", "/signup", "/script", "/me/api/json", "/api/json", "/computer/api/json",
+        "/env-vars.html", "/pluginManager/api/json", "/credentials/api/json", "/log/all", 
+        "/updateCenter/api/json","/queue/api/json", "/pluginManager/checkUpdatesServer/api/json",
+        "/pluginManager/api/json", "/whoAmI/api/json", "/crumbIssuer/api/json", "/overallLoad/api/json",
+        "/queue/api/json"
     ]
     accessible_endpoints = []
     for endpoint in common_endpoints:
@@ -136,24 +147,19 @@ def check_unauthenticated_endpoints(base_url):
             accessible_endpoints.append(full_url)
     return accessible_endpoints
 
-
-def dir_search(base_url, paths_file, auth=None):
+def dir_search(base_url, paths_file, auth=None, headers=None):
     accessible_paths = []
     try:
         with open(paths_file, "r") as file:
             paths = [line.strip() for line in file if line.strip()]
             for path in paths:
                 full_url = f"{base_url.rstrip('/')}/{path.lstrip('/')}"
-
-                # Pass 'auth' only if it's not None (i.e., if credentials are provided)
-                response = requests.get(full_url, auth=auth)
+                response = requests.get(full_url, auth=auth, headers=headers)
                 if response.status_code == 200:
                     accessible_paths.append(full_url)
     except FileNotFoundError:
         print(f"Directory paths file not found: {paths_file}")
     return accessible_paths
-
-
 
 def brute_force_credentials(base_url, user_list, pass_list):
     valid_credentials = []
@@ -165,30 +171,28 @@ def brute_force_credentials(base_url, user_list, pass_list):
                 valid_credentials.append((user, password))
     return valid_credentials
 
-
-def analyze_jenkins(base_url, auth):
+def analyze_jenkins(base_url, auth,headers):
     results = {
         "URL": base_url,
-        "Version": get_jenkins_version(base_url, auth),
-        "Auth Type": detect_auth_type(base_url, auth),
+        "Version": get_jenkins_version(base_url, auth, headers),
+        "Auth Type": detect_auth_type(base_url, auth, headers),
         "Anonymous Access": check_anonymous_access(base_url),
-        "CSRF Protection": check_csrf_protection(base_url, auth),
-        "Job Visibility": len(check_job_visibility(base_url, auth)),
-        "Script Console": check_script_console(base_url, auth),
-        "Node Details": len(check_nodes(base_url, auth)),
+        "CSRF Protection": check_csrf_protection(base_url, auth, headers),
+        "Job Visibility": len(check_job_visibility(base_url, auth, headers)),
+        "Script Console": check_script_console(base_url, auth, headers),
+        "Node Details": len(check_nodes(base_url, auth, headers)),
         "Uses HTTP": check_http_usage(base_url),
-        "Stored Credentials": check_stored_credentials(base_url, auth),
-        "Log Access": check_log_access(base_url, auth),
-        "Audit Logs Enabled": check_audit_logs(base_url, auth),
-        "API Access": check_api_access(base_url, auth),
-        "CLI Access": check_cli_access(base_url, auth),
+        "Stored Credentials": check_stored_credentials(base_url, auth, headers),
+        "Log Access": check_log_access(base_url, auth, headers),
+        "Audit Logs Enabled": check_audit_logs(base_url, auth, headers),
+        "API Access": check_api_access(base_url, auth, headers),
+        "CLI Access": check_cli_access(base_url, auth, headers),
         "Anonymous Node Access": check_anonymous_node_access(base_url),
         "Signup Allowed": check_signup_allowed(base_url),
         "UnAuth Endpoints": ", ".join(check_unauthenticated_endpoints(base_url)) or "None"
-
     }
     try:
-        node_data = check_nodes(base_url, auth)
+        node_data = check_nodes(base_url, auth,headers)
         if node_data["num_nodes"] > 0:
             node_details = ", ".join([f"{node['name']} (Architecture: {node['architecture']})" for node in node_data["nodes"]])
             results["Node Details"] = f"Total Nodes: {node_data['num_nodes']}. {node_details}"
@@ -199,7 +203,7 @@ def analyze_jenkins(base_url, auth):
         results["Node Details"] = "Error retrieving node details"
 
     # Check job visibility and only list if more than 0 jobs are found
-    jobs = check_job_visibility(base_url, auth)
+    jobs = check_job_visibility(base_url, auth, headers)
     if len(jobs) > 0:
         job_details = ", ".join([f"{job[0]} (url: {job[1]})" for job in jobs])
         results["Job Visibility"] = job_details
@@ -207,7 +211,7 @@ def analyze_jenkins(base_url, auth):
         results["Job Visibility"] = "None"
 
     # List stored credentials if more than 0
-    creds_details = check_stored_credentials(base_url, auth)
+    creds_details = check_stored_credentials(base_url, auth,headers)
     if creds_details:
         results["Stored Credentials"] = ", ".join(creds_details)
     else:
@@ -228,23 +232,26 @@ def save_to_csv(results, output_file):
         writer.writeheader()
         for result in results:
             writer.writerow(result)
-
 def main():
     parser = argparse.ArgumentParser(
         description="""Jenkins Vulnerability Scanner - A tool to scan Jenkins servers for common security issues.
 
-            Examples of usage:
-            1. Run the scanner with anonymous access:
-            python JenkinsVulnsFinder.py --url http://172.19.107.32:8080 --nocred 
+Examples of usage:
+# Run the scanner with anonymous access:
+python JenkinsVulnsFinder.py --url http://172.19.107.32:8080 --nocred 
 
-            2. Run the scanner with credentials:
-            python JenkinsVulnsFinder.py --url http://172.19.107.32:8080 --cred username:password 
+# Run the scanner with credentials:
+python JenkinsVulnsFinder.py --url http://172.19.107.32:8080 --cred username:password 
 
-            3. Run the scanner with brute-force enabled:
-            python JenkinsVulnsFinder.py --url http://172.19.107.32:8080/ --nocred --brute --users users.txt --pass pass.txt
+# Run the scanner with Cookie:
+python JenkinsVulnsFinder.py --url http://172.19.107.32:8080 --cookie "JSESSIONID.bc33f838=node0icu313myoabe17tcvkdlhkmez1.node0;"
 
-            4. Run Scanner with Directory/Path Search - Takes wordlist file as input
-            python JenkinsVulnsFinder.py --url http://172.19.107.32:8080 --nocred --dirb wordlist.txt
+# Run the scanner with brute-force enabled:
+python JenkinsVulnsFinder.py --url http://172.19.107.32:8080/ --nocred --brute --users users.txt --pass pass.txt
+
+# Run Scanner with Directory/Path Search - Takes wordlist file as input
+python JenkinsVulnsFinder.py --url http://172.19.107.32:8080 --nocred --dirb wordlist.txt
+python JenkinsVulnsFinder.py --url http://172.19.107.32:8080 --cred --dirb wordlist.txt
 
 """, formatter_class=argparse.RawTextHelpFormatter
     )
@@ -253,6 +260,7 @@ def main():
     parser.add_argument("--urls", help="File containing list of Jenkins URLs to check", required=False)
     parser.add_argument("--cred", help="Credentials in the format username:password", required=False)
     parser.add_argument("--nocred", action="store_true", help="Use anonymous access (no credentials required)", required=False)
+    parser.add_argument("--cookie", help="Cookie string for authentication", required=False)
     
     parser.add_argument("--output", help="Output CSV file to save results (default will be generated with timestamp)", required=False)
     
@@ -266,14 +274,19 @@ def main():
     
     args = parser.parse_args()
 
-    # Ensure either --cred or --nocred is passed along with --url or --urls
-    if not ((args.nocred or args.cred) and (args.url or args.urls)):
+    # Ensure either --cred, --nocred, or --cookie is passed along with --url or --urls
+    if not ((args.nocred or args.cred or args.cookie) and (args.url or args.urls)):
         parser.print_help()
-        print("\nERROR: Either --cred or --nocred must be specified, and either --url or --urls must be provided.")
+        print("\nERROR: Either --cred, --nocred, or --cookie must be specified, and either --url or --urls must be provided.")
         return
 
     auth = None
-    if args.nocred:
+    headers = {}
+
+    if args.cookie:
+        headers['Cookie'] = args.cookie
+        print("Using authentication via cookies...")
+    elif args.nocred:
         print("Using anonymous access...")
     elif args.cred:
         try:
@@ -283,7 +296,7 @@ def main():
             print("Invalid credentials format. Use username:password.")
             return
     else:
-        print("Either --cred or --nocred must be specified.")
+        print("Either --cred, --nocred, or --cookie must be specified.")
         return
 
     urls = []
@@ -331,10 +344,10 @@ def main():
     for url in urls:
         print(f"Analyzing {url}...")
         try:
-            result = analyze_jenkins(url, auth)
+            result = analyze_jenkins(url, auth, headers)
 
             # Fetch plugin details for CSV
-            plugins = check_plugins(url, auth)
+            plugins = check_plugins(url, auth, headers)
             plugin_details = "; ".join([f"{name} (v{version}, Update: {has_update})" for name, version, has_update in plugins])
             result["Plugins"] = plugin_details
 
@@ -343,7 +356,7 @@ def main():
                 result["Brute-force Credentials"] = ", ".join([f"{user}:{password}" for user, password in valid_credentials]) or "None"
 
             if args.dirb:
-                accessible_paths  = dir_search(url,args.dirb, auth)
+                accessible_paths  = dir_search(url,args.dirb, auth, headers)
                 if accessible_paths: 
                     for path in accessible_paths:
                         result["Accessible Paths"] = ", ".join(accessible_paths)
